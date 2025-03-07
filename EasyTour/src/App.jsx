7076e6d00a9ca52e6bc2527d2340d1d164011fce
Sameer Khatri <156842673+Sameerdev512@users.noreply.github.com>
2025-03-07 12:02:41 +0530

    Update Routers

--- EasyTour/src/App.jsx
+++ EasyTour/src/App.jsx
@@ -1,51 +1,51 @@
 // Import Components
-import './App.css'
-import Home from './Pages/Home/Home'
-import About from './Pages/About/About'
-import Gallery from './Pages/Gallary/Gallery'
-import Login from './Components/Login/Login';
-import Signup from './Components/Signup/Signup';
-import Group from './Components/Group/Group';
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Gallery from "./Pages/Gallary/Gallery";
+import Login from "./Components/Login/Login";
+import Signup from "./Components/Signup/Signup";
+import Group from "./Components/Group/Group";
 
 // Import React-Router-Dom
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
-
-  // React-router-Dom
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/about",
-      element: <About />,
-    },
-    {
-      path: "/gallery",
-      element: <Gallery />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/signup",
-      element: <Signup />,
-    },
-  ]);
+  // // React-router-Dom
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Home />,
+  //   },
+  //   {
+  //     path: "/about",
+  //     element: <About />,
+  //   },
+  //   {
+  //     path: "/gallery",
+  //     element: <Gallery />,
+  //   },
+  //   {
+  //     path: "/login",
+  //     element: <Login />,
+  //   },
+  //   {
+  //     path: "/signup",
+  //     element: <Signup />,
+  //   },
+  // ]);
 
   return (
-    <div>
-      {/*Use React-Route-Dom */}
-      <RouterProvider router={router} />
-      <Group/>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route >
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/gallery" element={<Gallery />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
